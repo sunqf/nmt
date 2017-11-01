1a7849dde232b73060aea086b73e724cb547a863
sunqf <guaiguai840422@163.com>
2017-11-01 18:40:02 +0800

    add pinyin dict

--- dict/format.py
+++ dict/format.py
@@ -10,9 +10,48 @@ from collections import namedtuple, defaultdict
 data_dir = '../han-dict'
 count = 0
 
-WordInfo = namedtuple("WordInfo", ["word", "synonym", "types"])
-
+WordInfo = namedtuple("WordInfo", ["word", "pinyins", "synonym", "types"])
+PinYin = namedtuple("PinYin", ['consonant', 'vowel', 'tone'])
 dict = defaultdict()
+
+start_index = len('xhziplay("')
+end_index = len('");')
+
+# 声母
+consonants = {'b', 'p', 'm', 'f',
+              'd',  't',  'n',  'l',
+              'g',  'k',  'h',  'x',
+              'j',  'q',  'y',  'w',
+              'zh', 'ch', 'sh', 'r',
+              'z',  'c',  's'}
+
+# 韵母
+# 单韵母（元音）
+single = {'a', 'o', 'e', 'i', 'u', 'ü'}
+# 复韵母
+multi = {'ai', 'ei', 'ao', 'ou', 'ia', 'ie', 'iao', 'iou', 'iu', 'ua', 'uo', 'uai', 'uei', 'ui', 'ue'}
+# 鼻韵母
+tone = {'an', 'en', 'ang', 'eng', 'ong',
+        'ian', 'in', 'iang', 'ing', 'iong',
+        'uan', 'uen', 'un', 'uang', 'ueng',
+        'üan', 'ün'}
+
+special = {'er'}
+
+vowels = single.union(multi).union(tone).union(special)
+print(vowels)
+
+
+def split_pinyin(pinyin):
+    if pinyin in vowels:
+        return '', pinyin
+    elif pinyin[0:2] in consonants and pinyin[2:] in vowels:
+        return pinyin[0:2], pinyin[2:]
+    elif pinyin[0:1] in consonants and pinyin[1:] in vowels:
+        return pinyin[0:1], pinyin[1:]
+    else:
+        raise Exception(pinyin + " can't split.")
+
 
 for name in os.listdir(data_dir):
     with open('%s/%s' % (data_dir, name)) as file:
@@ -20,6 +59,26 @@ for name in os.listdir(data_dir):
         data = BeautifulSoup(html, 'html.parser')
         word = data.find('td', class_='font_22').string
         definition = data.find('td', class_='font_18')
+
+        pinyins = []
+        try:
+            for p in ''.join([s.string for s in data.find('td', class_='font_14')]).split(','):
+                p = p.strip().replace('ɑ', 'a')
+                if len(p) > 0:
+
+                    fields = p.split(' ')
+                    if len(fields) == 1:
+                        c, v = split_pinyin(fields[0])
+                        pinyins.append(PinYin(c, v, ''))
+                    elif len(fields) == 2 and len(fields[1]) > start_index + end_index:
+                        pinyin = fields[1][start_index:-end_index]
+                        c, v = split_pinyin(pinyin[:-1])
+                        t = pinyin[-1]
+                        pinyins.append(PinYin(c, v, t))
+
+        except Exception as e:
+            print(name, word)
+            print(e)
 
         if definition is None:
             print('%s/%s' % (data_dir, name))
@@ -40,7 +99,7 @@ for name in os.listdir(data_dir):
         fields = list(filter(lambda node: node.startswith('【') if isinstance(node, NavigableString) else False, detail))
         types = set([f[1] for f in fields])
 
-        info = WordInfo(word, synonym_word, types)
+        info = WordInfo(word, pinyins, synonym_word, types)
 
         dict[word] = info
 
@@ -48,8 +107,12 @@ for word, info in dict.items():
     if info.synonym is not None and len(info.types) == 0:
         synonym = dict.get(info.synonym)
         if synonym is not None:
-            dict[word] = WordInfo(info.word, info.synonym, synonym.types)
+            dict[word] = WordInfo(info.word, info.pinyins, info.synonym, synonym.types)
             print('%s %s %s' % (word, synonym, str(synonym.types)))
 
 with open('word-type', 'w') as file:
-    file.writelines(['%s\t%s\t\n' % (info.word, ' '.join(info.types)) for _, info in dict.items()])
+    file.writelines(['%s\t%s\t%s\n' % (info.word,
+                                       ' '.join(info.types),
+                                       ' '.join(['%s|%s|%s' % (p.consonant, p.vowel, p.tone)
+                                                 for p in info.pinyins]))
+                     for _, info in dict.items()])
--- dict/name-dict.py
+++ dict/name-dict.py
@@ -5,20 +5,20 @@ import os
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-data_dir = 'han-dict'
+data_dir = 'name-dict'
 
 if os.path.exists(data_dir) is False:
     os.mkdir(data_dir)
 
-url_format = 'http://xh.5156edu.com/html3/%d.html'
-for id in tqdm(range(14674, 22526)):
+url_format = 'https://www.babble.com/baby-names/filter/raw/any/any/any/any/page/%d/'
+for id in tqdm(range(284, 286)):
     url = url_format % id
     try:
         data = requests.get(url)
         if data.status_code == 200:
-            soup = BeautifulSoup(data.content, 'html.parser', from_encoding='gbk')
-
+            soup = BeautifulSoup(data.content, 'html.parser', from_encoding='utf-8')
             with open('/'.join([data_dir, str(id)]), 'w') as f:
-                f.write(str(soup.find('table', style="word-break:break-all")))
+                f.write(str(soup))
+
     except Exception as e:
         print('%s failed. exception: %s' % (url, str(e)))
